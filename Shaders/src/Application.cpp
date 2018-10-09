08d261bddf2b3076d97cda78e6e54e92563006db
Tyler Kokoszka <techkoko77@gmail.com>
2018-10-09 09:48:01 -0400

    Added the GLEW library and linked it properly
    GLEW allows us to use the updated OpenGL lib

--- Shaders/src/Application.cpp
+++ Shaders/src/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -12,6 +14,7 @@ int main(void)
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
 	{
+		std::cout << "Error initializing GLFW!" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
@@ -19,11 +22,25 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	/* Initialize GLEW */
+	if (glewInit() != GLEW_OK) 
+	{
+		std::cout << "Error initializing GLEW!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);

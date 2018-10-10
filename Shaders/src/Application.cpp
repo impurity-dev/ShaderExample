b68658156d5dbdd73d7967fe6d456074862c7d61
Tyler Kokoszka <techkoko77@gmail.com>
2018-10-10 09:24:25 -0400

    Animate Color
    The color has a simple red channel animation

--- Shaders/src/Application.cpp
+++ Shaders/src/Application.cpp
@@ -207,14 +207,24 @@ int main(void)
 	// Pass our data to the shader uniform
 	GLCall(glUniform4f(location, 0.8f, 0.3f, 0.8f, 1.0f));
 
+	float r = 0.0f;
+	float increment = 0.05f;
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+		// Pass our data to the shader uniform
+		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 		// Draw our buffer
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+		if (r > 1.0f) increment = -0.05f;
+		else if (r < 0.0f) increment = 0.05f;
+		r += increment;
+
 
 
 		/* Swap front and back buffers */
